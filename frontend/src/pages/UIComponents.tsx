ec89530ec7c7bc33f858e52871532467689bdc66
Daniel Hrynusiw <64728456+Daniel88dev@users.noreply.github.com>
2024-03-16 19:08:59 +0100

    Update dependencies in package-lock.json
    Several dependencies were added, updated, and removed in package-lock.json. These modifications primarily focus on the frontend functionality, ensuring that the necessary dependencies are available and up-to-date for front-end development. This includes the addition of new packages like "@radix-ui/react-popover" and "date-fns" amongst others. Moreover, it includes the update to a different version of the "clsx" package and overall fine-tuning to improve the project's performance and stability.

--- frontend/src/pages/UIComponents.tsx
+++ frontend/src/pages/UIComponents.tsx
@@ -4,11 +4,22 @@ import FilledButton from "../components/UI/Buttons/FilledButton.tsx";
 import CloseButton from "../components/UI/Buttons/CloseButton.tsx";
 import InputText from "../components/UI/Input/InputText.tsx";
 import ModalFull from "../components/UI/ModalFull.tsx";
+import Calendar from "../components/UI/CalendarPicker/Calendar.tsx";
+import { format } from "date-fns";
+import { cn } from "../components/utility/cn.ts";
+import { Calendar as CalendarIcon } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../components/UI/CalendarPicker/Popover.tsx";
+import { ButtonShadnCn } from "../components/UI/Buttons/ButtonShadnCn.tsx";
 
 const UIComponents = () => {
   const notifyRef = useRef<ChildMethods>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const [displayModal, setDisplayModal] = useState(false);
+  const [date, setDate] = useState<Date | undefined>(new Date());
 
   const openModal = () => {
     setDisplayModal(true);
@@ -51,6 +62,8 @@ const UIComponents = () => {
 
   const initial = "test";
 
+  console.log(date);
+
   return (
     <>
       <h1>Problem Lists</h1>
@@ -73,6 +86,28 @@ const UIComponents = () => {
         />
       </div>
       <FilledButton onClick={textInInput}>Test Input</FilledButton>
+      <Popover>
+        <PopoverTrigger asChild>
+          <ButtonShadnCn
+            variant={"outline"}
+            className={cn(
+              "w-[280px] justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className={"mr-2 h-4 w-4"} />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </ButtonShadnCn>
+        </PopoverTrigger>
+        <PopoverContent className={"w-auto p-0"}>
+          <Calendar
+            mode={"single"}
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
       {displayModal && (
         <ModalFull onClose={closeModal} title={"Test of Modal"} type="medium">
           {modalContent}
